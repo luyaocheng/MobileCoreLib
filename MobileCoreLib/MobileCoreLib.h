f28f0aff8929786dce126a98836c4739dae500a8
luyaocheng <luyaocheng@gmail.com>
2016-04-18 20:27:21 -0400

    Initial check in

---
+++ MobileCoreLib/MobileCoreLib.h
@@ -0,0 +1,19 @@
+//
+//  MobileCoreLib.h
+//  MobileCoreLib
+//
+//  Created by Luyao Cheng on 4/18/16.
+//  Copyright © 2016 Luyao Cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MobileCoreLib.
+FOUNDATION_EXPORT double MobileCoreLibVersionNumber;
+
+//! Project version string for MobileCoreLib.
+FOUNDATION_EXPORT const unsigned char MobileCoreLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobileCoreLib/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'MobileCoreLib' do
+	pod "AFNetworking", '~> 3.1'
+end
+
+target 'MobileCoreLibTests' do
+
+end
+
